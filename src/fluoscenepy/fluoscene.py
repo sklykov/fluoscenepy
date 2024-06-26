f6e92f5e3a29e0a328d77353069b0a5506bdd71b
sklykov <sergej.klykow@gmail.com>
2024-06-26 18:24:06 +0200

    Expanded docstrings for the classes

--- src/fluoscenepy/fluoscene.py
+++ src/fluoscenepy/fluoscene.py
@@ -12,16 +12,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings
 from typing import Union
+import random
 
 # %% Local imports
 
 
 # %% Scene (image) class def.
 class UscopeScene():
-    """Replicate the microscopic image (frame or scene)."""
+    """
+    Replicate the common fluorescences microscopic image (frame or 'scene').
+
+    This class simulates the bright objects with round and elongate shapes as the basic examples of imaged objects. \n
+    The ratio behind development of this class - to get the ground truth images for image processing workflows tests. \n
+    For more complex and useful cases, please, check the References.
+
+    References
+    ----------
+    [1]
+
+    """
 
     # Class parameters
-    width: int; height: int; __warn_message = ""; max_pixel_value = 255; img_type = np.uint8
+    width: int; height: int; __warn_message: str = ""; max_pixel_value = 255; img_type = np.uint8
     acceptable_img_types = ['uint8', 'uint16', 'float', np.uint8, np.uint16, np.float64]
 
     def __init__(self, width: int, height: int, img_type: Union[str, np.uint8, np.uint16, np.float64] = 'uint8'):
@@ -50,8 +62,8 @@ class UscopeScene():
         # Check provided width and height
         if width < 2 or height < 2:
             raise ValueError(f"Provided dimensions ({width}x{height}) is less than 2")
-        if width > 5000 or height > 5000:
-            __warn_message = f"Provided dimensions ({width}x{height}) are unrealistic for the image"
+        if width > 10000 or height > 10000:
+            __warn_message = f"Provided dimensions ({width}x{height}) pixels are unrealistic for the common image"
             warnings.warn(__warn_message)
         self.width = width; self.height = height
         # Check an image type
@@ -66,19 +78,28 @@ class UscopeScene():
         self.image = np.zeros(shape=(height, width), dtype=self.img_type)
 
     # %% Scene manipulation
-    def show_scene(self):
+    def show_scene(self, str_id: str = ""):
         """
-        Show interactively the stored in the class scene (image).
+        Show interactively the stored in the class scene (image) by plotting it using matplotlib.
+
+        Parameters
+        ----------
+        str_id : str, optional
+            Unique string id for plotting several plots. The default is "".
 
         Returns
         -------
         None.
 
         """
-        plt.close('all')
+        plt.close('all'); h, w = self.image.shape
+        height_width_ratio = h/w; default_image_size = 5.8
+        if len(str_id) == 0:
+            str_id = str(random.randint(1, 100))
         if not plt.isinteractive():
             plt.ion()
-        plt.figure("UscopeScene"); plt.imshow(self.image, cmap=plt.cm.viridis, origin='upper'); plt.tight_layout()
+        plt.figure("UscopeScene_"+str_id, figsize=(default_image_size, default_image_size*height_width_ratio))
+        plt.imshow(self.image, cmap=plt.cm.viridis, origin='upper'); plt.axis('off'); plt.tight_layout()
 
     def clear_scene(self):
         """
@@ -94,7 +115,21 @@ class UscopeScene():
 
 # %% Object class definition
 class FluoObj():
-    pass
+    """
+    Modelling the fluorescent bright object with the specified type.
+
+    It can be used for embedding it to the 'UscopeScene' class for building up the microscopic image.
+
+    """
+
+    # Class parameters
+    shape_type: str; border_type: str; __acceptable_shape_types: list = ['round', 'r', 'elongated', 'curved']
+
+    def __init__(self, shape_type: str = 'round', border_type: str = 'precise'):
+        if shape_type in self.__acceptable_shape_types:
+            self.shape_type = shape_type
+        else:
+            raise ValueError(f"Provided shape type {shape_type} not in acceptable list {self.__acceptable_shape_types}")
 
 
 # %% Some tests
